49374b194d0e4388ad730da6c03a6d9c3db34446
Dani <daniel.dona@alu.uclm.es>
2016-12-20 21:00:34 +0100

    get/set mesa

--- Static_view/Class_Diagram/Domain/Static_view/Class_Diagram/Domain/Mesa.java
+++ Static_view/Class_Diagram/Domain/Static_view/Class_Diagram/Domain/Mesa.java
@@ -8,5 +8,53 @@ public class Mesa {
 	private boolean reservada;
 //	private string hora;
 	private boolean atendida;
+	
+	public void setidmesa(int idmesa){
+		
+		this.idmesa = idmesa;
+		
+	}
+	
+	public void setocupada(boolean ocupada){
+		
+		this.ocupada = ocupada;
+		
+	}
 
+	public void setreservada(boolean reservada){
+		
+		this.reservada = reservada;
+		
+	}
+	
+	public void setatendida(boolean atendida){
+		
+		this.atendida = atendida;
+		
+	}
+	
+	public int getidmesa(){
+		
+		return this.idmesa;
+		
+	}
+	
+	public boolean getocupada(){
+		
+		return this.ocupada;
+		
+	}
+	
+	public boolean getreservada(){
+		
+		return this.reservada;
+		
+	}
+	
+	public boolean getatendida(){
+		
+		return this.atendida;
+		
+	}
+	
 }
